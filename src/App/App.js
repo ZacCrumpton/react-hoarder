d0b582542115763d19c51785b7d8fc7f38515e93
Zac Crumpton <z.crumpton@gmail.com>
2020-05-18 21:18:08 -0500

    fixed App.js

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.scss';
 
-function App() {
-  return (
+class App extends React.Component {
+  render() {
+    return (
     <div className="App">
     <h2>React Hoarder</h2>
     <button className="btn btn-dark"><i className="fas fa-rocket"></i></button>
   </div>
-  );
+    );
+  }
 }
 
 export default App;

818b6fc51493d8e6b5c17b5a95714bcf25678d2e
Zac Crumpton <z.crumpton@gmail.com>
2020-06-08 20:40:36 -0500

    auth works

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,41 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
 import './App.scss';
 
+import Auth from '../components/pages/Auth/Auth';
+import MyNavbar from '../components/shared/MyNavbar/MyNavbar';
+
+import fbConnection from '../helpers/data/connection';
+
+fbConnection();
+
 class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
+  componentDidMount() {
+    this.removeListner = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListner();
+  }
+
   render() {
     return (
     <div className="App">
-    <h2>React Hoarder</h2>
-    <button className="btn btn-dark"><i className="fas fa-rocket"></i></button>
+      <MyNavbar />
+    <h1>React Hoarder</h1>
+    <Auth />
   </div>
     );
   }
